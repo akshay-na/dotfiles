dc28ce7e4f7954f5ea677726c14027b466894493
akshay-na <akshayna04@gmail.com>
2024-11-01 02:00:09 +0530

    add vim as global for lsp

--- lua/plugins/configs/lsp-zero.lua
+++ lua/plugins/configs/lsp-zero.lua
@@ -43,6 +43,30 @@ for server_name, filetypes in pairs(servers) do
   })
 end
 
+-- Define custom settings for the Lua language server
+lsp.configure('lua_ls', {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (LuaJIT in Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false, -- Avoids prompt about third-party workspace libraries
+      },
+      telemetry = {
+        enable = false, -- Disable telemetry for privacy
+      },
+    },
+  },
+})
+
 
 -- Enable caching for LSP handlers (preserving the definition reuse functionality)
 vim.lsp.handlers["textDocument/definition"] = vim.lsp.with(vim.lsp.handlers.definition, {
