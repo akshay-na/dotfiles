0c8f607a7ee14be3b65632183e3afa4bf59955c1
akshay-na <akshayna04@gmail.com>
2025-06-19 21:36:27 +0530

    fix: stowing of installation files

--- install/macos.sh
+++ install/macos.sh
@@ -5,6 +5,8 @@ GREEN="\033[1;32m"
 YELLOW="\033[1;33m"
 RESET="\033[0m"
 
+export PATH=$PATH:/opt/homebrew/bin
+
 # Function to echo with color
 echo_with_color() {
   echo -e "${1}${2}${RESET}"
@@ -12,22 +14,18 @@ echo_with_color() {
 
 install_macos() {
   echo_with_color "$YELLOW" "Detected macOS..."
-  echo_with_color "$YELLOW" "Setting Personalized macOS defaults..."
-  source "$DOTFILES_DIR/install/default_mac_settings.sh"
 
   # Ensure Homebrew is installed
   if ! command -v brew >/dev/null 2>&1; then
     echo_with_color "$YELLOW" "Homebrew not found. Installing..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    source $HOME/.zshrc
   fi
 
   echo_with_color "$YELLOW" "Installing fonts..."
 
   brew install --cask \
     font-caskaydia-cove-nerd-font \
-    font-caskaydia-mono-nerd-font \
-    font-caskaydia-mono-nerd-font-mono
+    font-caskaydia-mono-nerd-font
 
   echo_with_color "$YELLOW" "Installing CLI & Utilities via Brew..."
 
@@ -52,15 +50,16 @@ install_macos() {
     alacritty \
     cursor \
     chatgpt \
-    google-chrome \
     caffeine \
     grammarly-desktop \
-    bitwarden \
     spotify \
     dbgate \
     postman \
     hiddenbar \
     espanso
+
+  echo_with_color "$YELLOW" "Setting Personalized macOS defaults..."
+  source "$DOTFILES_DIR/install/default_mac_settings.sh"
 
   echo_with_color "$GREEN" "macOS setup complete!"
 }
