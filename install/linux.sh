4499fab689cf15ebbbd7ccc33843b7f0ce664566
akshay-na <akshayna04@gmail.com>
2025-06-28 00:08:03 +0530

    refactor: consolidated packages installation in linux

--- install/linux.sh
+++ install/linux.sh
@@ -11,18 +11,6 @@ echo_with_color() {
 }
 
 install_linux() {
-  echo_with_color "$YELLOW" "Detected Linux with apt..."
-
-  echo_with_color "$YELLOW" "Updating apt and installing base packages..."
-  sudo apt update
-  sudo apt install -y gpg curl git zsh unzip stow make gcc wget ripgrep eza bat tmux zoxide fzf coreutils
-
-  # --- bat (aliased as cat) ---
-  echo_with_color "$YELLOW" "Installing bat..."
-  # Some distros call the binary 'batcat'
-  if [[ ! -x "$(command -v bat)" && -x "$(command -v batcat)" ]]; then
-    sudo ln -sf /usr/bin/batcat /usr/local/bin/bat
-  fi
-
+  echo_with_color "$YELLOW" "Detected Linux..."
   echo_with_color "$GREEN" "Linux setup complete!"
 }
